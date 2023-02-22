b1dd35204074b5fcdcdace4ecd56ac5e781534cf
D.J. Crosby <taladan@gmail.com>
2023-02-22 11:02:00 -0500

    Finalize game functionality and styling
    Ton of updates (I need to remember to commit more often!):
    
    - Page styling - Laid out all the page elements to work on a browser
    - Rainbow functionality - Toggleable button for rainbow painting
    - Gradient button settings - Neat stuff from SO with linear-Gradient
    - logical positioning of html
    - general comments cleaned up and code (sort of) organized

--- scripts/etchasketch.js
+++ scripts/etchasketch.js
@@ -1,35 +1,12 @@
-// # Assignment
+// set inital rainbow
+let rainbow = false;
 
-// Create a webpage with a 16x16 grid of square divs.
-// Create the divs using JavaScript. Don’t try making them by hand with copy and pasting in your HTML file!
-// There are several different ways to make the divs appear as a grid (versus just one on each line). Feel free to use any or play with each of them:
-// - float/clear
-// - inline-block
-// - flexbox
-// - CSS Grid
-
-// Be careful with borders and margins, as they can adjust the size of the squares!
-
-// Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
-// Hint: “Hovering” is what happens when your mouse enters a div and ends when your mouse leaves it. You can set up event listeners for either of those events as a starting point.
-// There are multiple ways to change the color of the divs, including:
-// adding a new class to the div.
-// changing the div’s background color using JavaScript.
-// Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid. Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. Tip: Set the limit for the user input to a maximum of 100. A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
-// Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
-// Also check out prompts.
-// You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used.
-// Push your project to GitHub!
-
-// Extra Credit
-
-// Instead of just changing the color of a square from black to white (for example), have each pass through with the mouse change it to a completely random RGB value. Then try having each pass just add another 10% of black to it so that only after 10 passes is the square completely black.
+// set frame to max gameboard size
+const frame = document.getElementById("frame");
+frame.style.width = "449px";
+frame.style.height = "449px";
 
 // Create container div
-
-const title = document.querySelector("h1");
-const frame = document.getElementById("frame");
-
 const container = document.createElement("div");
 container.id = "container";
 container.style.margin = "0 auto";
@@ -48,6 +25,13 @@ const initialDivCount = 16;
 let currentDivCount = initialDivCount;
 
 // create div count selector
+const divCountTitle = document.createElement("h2");
+divCountTitle.style.display = "flex";
+divCountTitle.style.flexDirection = "column";
+divCountTitle.style.justifyContent = "space-apart";
+divCountTitle.textContent = "Adjust Screen Size";
+
+// Size picker
 const divCountSlider = document.createElement("input");
 divCountSlider.id = "sizer";
 divCountSlider.type = "range";
@@ -55,59 +39,69 @@ divCountSlider.setAttribute("min", initialDivCount);
 divCountSlider.setAttribute("max", initialDivCount * 4);
 divCountSlider.setAttribute("value", initialDivCount);
 divCountSlider.setAttribute("step", initialDivCount);
+divCountSlider.addEventListener("mouseup", getSliderValue);
+
+// Rainbow Button;
+const rainbowButton = document.createElement("button");
+rainbowButton.addEventListener("click", toggleRainbow);
+rainbowButton.textContent = "Taste the Rainbow";
 
 // Place count selector in page
 const tools = document.getElementById("tools");
-tools.appendChild(divCountSlider);
-divCountSlider.addEventListener("mouseup", getSliderValue);
+divCountTitle.appendChild(divCountSlider);
+tools.appendChild(divCountTitle);
+tools.appendChild(rainbowButton);
 
 // Track currentDivCount
 function getSliderValue(e) {
   currentDivCount = e.target.value;
   createGameBoard();
-
-  // Need to change grid when count changes
 }
 
 // create divs //
-
 function createGameBoard() {
   // clear the board first
   container.replaceChildren();
-  const containerWidth = parseInt(container.style.width);
-  const containerHeight = parseInt(container.style.height);
-  const divCount = parseInt(currentDivCount);
 
+  // build rows
   for (let row = 0; row < currentDivCount; row++) {
     const currentRow = document.createElement("div");
     currentRow.id = `row-${row}`;
     currentRow.className = "row";
     currentRow.maxHeight = container.style.height / currentDivCount;
     currentRow.maxWidth = container.style.width / currentDivCount;
+    // build cells
     for (let column = 0; column < currentDivCount; column++) {
       const currentCell = document.createElement("span");
-      currentCell.style.backgroundColor = "#ccc";
-      currentCell.style.border = "1px solid #ddd";
       currentCell.id = `${currentRow.id}-${column}`;
       currentCell.className = "cell";
-      currentCell.style.display = "Block";
       currentCell.addEventListener("mouseover", setCellColor);
+      currentCell.style.backgroundColor = "#ccc";
+      currentCell.style.border = "1px solid #ddd";
+      currentCell.style.display = "Block";
       currentCell.style.maxWidth = "6px";
       currentCell.style.maxHeight = "6px";
       currentCell.textContent = "&nbsp";
       currentCell.style.color = "transparent";
       currentCell.style.userSelect = "none";
+
+      // add each cell to row
       currentRow.append(currentCell);
     }
+    // add each row to container
     container.appendChild(currentRow);
     container.style.transform = `translate(${Math.floor(
       parseInt(container.style.width) / parseInt(currentDivCount)
     )}px, ${Math.floor(
       parseInt(container.style.height) / parseInt(currentDivCount)
     )}px)`;
-    // console.log(container.style.transform);
     frame.appendChild(container);
   }
+}
+
+// Calculate frame size
+function calculateSize(cWidth, cHeight, tWidth, tHeight) {
+  return [cWidth + tWidth, cHeight + tHeight];
 }
 
 /* Pick pen color */
@@ -120,10 +114,13 @@ let currentPenColor = initialPenColor;
 
 function setCellColor(e) {
   let cell = document.getElementById(e.target.id);
+  if (rainbow === true) {
+    currentPenColor = randomRgb();
+  }
   cell.style.backgroundColor = currentPenColor;
 }
 
-/* May use this later - for now it's crufty */
+/* Set up color picker */
 //
 //
 const colorPicker = new iro.ColorPicker("#picker", {
@@ -133,10 +130,35 @@ const colorPicker = new iro.ColorPicker("#picker", {
 const picker = document.getElementById("picker");
 picker.addEventListener("mouseleave", setPen);
 
-// create color selector
+// color selector
 function setPen() {
-  console.log("Setting color!");
   currentPenColor = colorPicker.color.hexString;
 }
+
+function toggleRainbow() {
+  if (!rainbow) {
+    rainbow = true;
+  } else {
+    setPen();
+    rainbow = false;
+  }
+}
+function randomRgb() {
+  let o = Math.round;
+  let r = Math.random;
+  let s = 255;
+  return (
+    "rgba(" +
+    o(r() * s) +
+    "," +
+    o(r() * s) +
+    "," +
+    o(r() * s) +
+    "," +
+    r().toFixed(1) +
+    ")"
+  );
+}
+
 /* Create the gameboard for inital page view after pageload */
 createGameBoard();
